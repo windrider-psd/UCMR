03bb3cc89a7706ea843a40fb9e2033934bccd41f
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-05-09 13:00:46 -0300

    Correção da versão

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.locals.autor = "UFSM"
-app.locals.versao = "0.3.0";
+app.locals.versao = "0.3.1";
 app.locals.anoAtual = new Date().getFullYear();
 
 if(typeof(process.argv[2]) !== 'undefined' && (process.argv[2].toString().toLowerCase() == 'd' || process.argv[2].toString().toLowerCase() == 'debug'))

a5045b73895d642af463350686dbeafc18442908
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-05-23 11:10:15 -0300

    Versão II

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.locals.autor = "UFSM"
-app.locals.versao = "0.4.0";
+app.locals.versao = "0.5.0";
 app.locals.anoAtual = new Date().getFullYear();
 if(argv.debug)
 {

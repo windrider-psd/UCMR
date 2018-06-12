b8b4cb4c4c20330d513910cfbb9e5909f8b13cf5
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-06-12 09:02:22 -0300

    try catch SolarGetter

--- modulos/SolarGetter.js
+++ modulos/SolarGetter.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 var PainelSolar = require('./../models/db/PainelSolar');
-var moment = require('moment');
+var LogEventos = require('./../models/db/LogEventos')
 var yargs = require('yargs').argv;
 var http = require('http');
 mongoose.connect(yargs.mongourl);
@@ -11,63 +11,70 @@ async function SalvarLog(objeto) //Objeto PainelSolar
 {
     var energiaAgora;
     var tempoagora = new Date();
-    var valor;
-    if(objeto.tipo != 0)
+
+    try
     {
-        var options = {
-            hostname: objeto.host,
-            port: 80,
-            path: objeto.path,
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' }
-            };
-            
-            var req = http.request(options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function (data) {
-                var obj = JSON.parse(data);
-                energiaAgora = obj.Body.Data.PAC.Value;
-                objeto.logs.push({valor : energiaAgora, tempo : tempoagora});
-                objeto.save(function(err)
-                {
-                    if(err) throw err;
-                    else
-                    {
-                        process.send({id : objeto._id, valor : energiaAgora, tempo : tempoagora});
-                    }
-                });
+        if(objeto.tipo != 0)
+        {
+            var options = {
+                hostname: objeto.host,
+                port: 80,
+                path: objeto.path,
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' }
+                };
                 
+                var req = http.request(options, function(res) {
+                res.setEncoding('utf8');
+                res.on('data', function (data) {
+                    var obj = JSON.parse(data);
+                    energiaAgora = obj.Body.Data.PAC.Value;
+                    objeto.logs.push({valor : energiaAgora, tempo : tempoagora});
+                    objeto.save(function(err)
+                    {
+                        if(err) throw err;
+                        else
+                        {
+                            process.send({id : objeto._id, valor : energiaAgora, tempo : tempoagora});
+                        }
+                    });
+                    
 
-            });
-            });
-            req.on('error', function(e) {
-            throw e.message;
-            });
-            req.end();
-    }
-    else
-    {
-        energiaAgora = (typeof(objeto.logs[objeto.logs.length - 1]) != 'undefined') ? objeto.logs[objeto.logs.length - 1].valor : 0;
-        var variacao = Math.floor(Math.random() * (400 - (-400) + 1) + (-400));
-        energiaAgora += variacao;
-        if(energiaAgora < 0)
-        {
-            energiaAgora = 0;
+                });
+                });
+                req.on('error', function(e) {
+                throw e.message;
+                });
+                req.end();
         }
-        
-        objeto.logs.push({valor : energiaAgora, tempo :tempoagora});
-        objeto.save(function(err)
+        else
         {
-            if(err) throw err;
-            else
+            energiaAgora = (typeof(objeto.logs[objeto.logs.length - 1]) != 'undefined') ? objeto.logs[objeto.logs.length - 1].valor : 0;
+            var variacao = Math.floor(Math.random() * (400 - (-400) + 1) + (-400));
+            energiaAgora += variacao;
+            if(energiaAgora < 0)
             {
-                process.send({id : objeto._id, valor : energiaAgora, tempo : tempoagora});
+                energiaAgora = 0;
             }
-        });
-        
-    }
+            
+            objeto.logs.push({valor : energiaAgora, tempo :tempoagora});
+            objeto.save(function(err)
+            {
+                if(err) throw err;
+                else
+                {
+                    process.send({id : objeto._id, valor : energiaAgora, tempo : tempoagora});
+                }
+            });
+            
+        }
 
-    return energiaAgora;
+        return energiaAgora;
+    }
+    catch(err)
+    {
+        new LogEventos({tempo : new Date(), evento : "Erro ao capturar dados do painel solar: " + objeto.nome + "(" + objeto._id + ")" }).save();
+    }
 } 
 
     

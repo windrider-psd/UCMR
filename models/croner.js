aad1725027c4eee9e648be85fe71ead502dd91da
Christian Luã Lemos <christianlualemosc@hotmail.com>
2018-10-16 16:17:02 -0300

    [master] Correção URI model em Croner

--- models/croner.js
+++
@@ -1,8 +0,0 @@
-var cron = require('node-cron');
-var LogEventos = require('./DBModels').LogEventos
-module.exports = cron.schedule('0 0 0 * * *', () => {
-    LogEventos.deleteMany({}, function(err) {
-        if(err)
-            console.log(err)
-    })
-})
--- services/Croner.js
+++ services/Croner.js
@@ -1,5 +1,5 @@
 let cron = require('node-cron');
-let LogEventos = require('./db/LogEventos');
+let LogEventos = require('./../models/DBModels').LogEventos;
 module.exports = cron.schedule('0 0 0 * * *', () => {
     LogEventos.deleteMany({}, function(err) {
         if(err)
